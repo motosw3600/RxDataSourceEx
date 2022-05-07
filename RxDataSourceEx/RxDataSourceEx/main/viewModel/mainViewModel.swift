eef448cc1755a1ab920b20ab80510885969afaa0
motosw3600 <motosw3600@gmail.com>
2022-05-07 15:50:07 +0900

    feat: MainViewModel add

---
+++ RxDataSourceEx/RxDataSourceEx/main/viewModel/mainViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  mainViewModel.swift
+//  RxDataSourceEx
+//
+//  Created by 박상우 on 2022/05/07.
+//
+
+import Foundation
+
+protocol MainViewModelProtocol {
+    
+}
+
+struct MainViewModel: MainViewModelProtocol {
+    
+}
